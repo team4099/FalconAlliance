f3a774344adec3cd9d4b14a3a1c4b0219831117c
Shom770 <82843611+Shom770@users.noreply.github.com>
2022-10-01 17:53:01 -0400

    add event code parameter to min function

--- falcon_alliance/schemas/main_schemas.py
+++ falcon_alliance/schemas/main_schemas.py
@@ -1194,21 +1194,27 @@ class Team(BaseSchema):
         )
         return [Media(**social_media_info) for social_media_info in response]
 
-    def min(self, year: typing.Union[range, int], metric: Metrics) -> typing.Union[Match, typing.Tuple[float, Event]]:
+    def min(
+        self, year: typing.Union[range, int], metric: Metrics, *, event_code: typing.Optional[str] = None
+    ) -> typing.Union[Match, typing.Tuple[float, Event]]:
         """
         Retrieves the minimum of a certain metric based on the year.
 
         Args:
             year (range, int): An integer representing the year to apply the metric to or a range object representing the years to apply the metric to.
             metric (Metrics): An Enum object representing which metric to use to find the minimum of something relating to a team of your desire.
+            event_code(str, optional): A string representing which event to apply a certain metric to for finding the minimum based on said metric (optional).
 
         Returns:
             typing.Union[Match, tuple[float, falcon_alliance.Event]]: A Match object representing the match with the minimum score if Metrics.MATCH_SCORE is passed into `metric` or a tuple containing the minimum OPR/DPR/CCWM for a team and the event where the team had said minimum OPR/DPR/CCWM if Metrics.OPR, Metrics.DPR or Metrics.CCWM is passed into `metric`.
         """  # noqa
         if metric == Metrics.MATCH_SCORE:
-            team_matches = self.matches(year)
+            team_matches = self.matches(year, event_code) if event_code else self.matches(year)
             return min(team_matches, key=lambda match: match.alliance_of(self.key).score)
         elif metric in {Metrics.OPR, Metrics.DPR, Metrics.CCWM}:
+            if event_code:
+                raise ValueError(f"`event_code` parameter incompatible with the metric {metric}.")
+
             team_oprs = []
 
             for event in self.events(year):
