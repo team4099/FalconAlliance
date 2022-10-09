4002eb823c90e2e21ce56055ad577d0644348d2f
Shom770 <82843611+Shom770@users.noreply.github.com>
2022-10-08 23:20:30 -0400

    remove appliedfunction -- user should do it themselves

--- falcon_alliance/plotting/functions.py
+++ falcon_alliance/plotting/functions.py
@@ -1,31 +1,11 @@
 import collections.abc
 import typing
 from itertools import zip_longest
-from operator import attrgetter, itemgetter
 
 import matplotlib.pyplot as plt
 
 
-class AppliedFunction:
-    """Represents an applied function, utilized for plotting."""
-
-    def __init__(self, applied_result: list):
-        self._applied_result = applied_result
-
-    def __getitem__(self, item):
-        return AppliedFunction([*map(itemgetter(item), self._applied_result)])
-
-    def __getattr__(self, item):
-        return AppliedFunction([*map(attrgetter(item), self._applied_result)])
-
-    def __iter__(self):
-        return iter(self._applied_result)
-
-    def __str__(self):
-        return str(self._applied_result)
-
-
-def apply(function: typing.Callable, **kwargs) -> AppliedFunction:
+def apply(function: typing.Callable, **kwargs) -> list:
     """
     Applies keyword arguments to the function passed in, utilized for plotting.
 
@@ -34,7 +14,7 @@ def apply(function: typing.Callable, **kwargs) -> AppliedFunction:
         **kwargs: The keyword argument name is the corresponding keyword argument for the function and the value is either a certain value that tells the function to keep that value constant or an iterable representing the different values to call the function with.
 
     Returns:
-        AppliedFunction: A custom class containing the list with the results of the applied function -- this instance can be used to pick out a specific attribute or a certain index for each element.
+        list: A list of all the return values of the function based on the values that were applied to the function.
     """  # noqa
     kwargs_constant = {name: value for name, value in kwargs.items() if not isinstance(value, collections.abc.Iterable)}
     kwargs_iterables = {name: value for name, value in kwargs.items() if name not in kwargs_constant.keys()}
@@ -44,7 +24,7 @@ def apply(function: typing.Callable, **kwargs) -> AppliedFunction:
     for zipped in zip_longest(*kwargs_iterables.values()):
         formatted_kwargs.append({name: value for name, value in zip(kwargs_iterables.keys(), zipped)})
 
-    return AppliedFunction([function(**kwargs_constant, **fmt_kwargs) for fmt_kwargs in formatted_kwargs])
+    return [function(**kwargs_constant, **fmt_kwargs) for fmt_kwargs in formatted_kwargs]
 
 
 def to_plot(
